48941e383e35338cbe2061f64309577b04a13c6f
Sonali Shukla <sonalikatara@gmail.com>
2018-06-06 20:38:51 -0700

    feat: add custom styles to status bar

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from 'react'
-import { View , Text } from 'react-native'
+import { View , Text, StatusBar } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import History from './components/History'
+import { Constants } from 'expo'
+import { purple } from './utils/colors'
 
+function UdaciStatusBar ({backgroundColor, ...props}){
+  return(
+  <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+       <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+  </View>
+  )
+}
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <History />
+          <UdaciStatusBar backgroundColor={purple} barStyle="light-content" />
         </View>
       </Provider>
     )
